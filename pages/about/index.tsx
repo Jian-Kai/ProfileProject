6b8ca421c8ccc6e899aa230c265a657cc113ffe8
JianKai <tony14256@gmail.com>
2020-08-15 15:04:09 +0800

    add banner

---
+++ pages/about/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Haed from 'next/head';
+import style from '@styles/index/styles.module.scss';
+import NavBar from '@components/NavBar';
+import links from '@public/links.json';
+
+const Index: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Haed>
+        <title>About Me</title>
+      </Haed>
+      <div>
+        <NavBar title="Jian-Kai" links={links.links} />
+        <h1 className={style.title}>Wellcome to My Profile Site 123456</h1>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Index;
